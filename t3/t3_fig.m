0b77f52d0ffa7f7ceef115e99870d708814c27bc
taiwan-jjl <taiwan.jjl@gmail.com>
2022-09-19 17:42:39 +0200

    sync

--- t3/t3_fig.m
+++ t3/t3_fig.m
@@ -3,8 +3,8 @@ plot(N_O2, bw_O2, "-x", N_O3, bw_O3, "-x");
 xlabel("# of floats");
 ylabel("GBytes/sec");
 title("stream\_triad");
-L1 = 288*1024/4;
-L2 = 7.5*1024*1024/4;
+L1 = 48*1024/4;
+L2 = 1.25*1024*1024/4;
 L3 = 18 *1024*1024/4;
 xline(L1/3);
 xline(L2/3);
@@ -14,7 +14,7 @@ yline(23.464);
 legend("O2","O3","L1d=288KB","L2=7.5MB","L3=18MB", "DDR4-2ch=46.928GB/s", "DDR4-1ch=23.464GB/s");
 %xlim([L2/3*0.8 L3/3*1.3]);
 %xlim([L1/3*0.8 L2/3*1.3]);
-%xlim([0/3*0.5 L1/3*1.3]);
+xlim([0/3*0.5 L1/3*1.3]);
 %xlim([0/3*0.5 L1/3*0.01]);
 
 
